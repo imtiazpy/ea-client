b46d960b747eb46ce4a3d34aeb11783ee96928d7
imtiazpy <ahmed.imti1994@gmail.com>
2022-10-16 16:47:00 +0600

    refactor: refactored deleteAllCookies method in interceptor

--- src/utility/interceptor.ts
+++ src/utility/interceptor.ts
@@ -7,7 +7,7 @@ export const NEXT_PUBLIC_APP_API_URL = process.env.NEXT_PUBLIC_APP_API_URL || 'h
 
 export const deleteAllCookies = () => {
     document.cookie.split(";").forEach(function(c) {
-        document.cookie = c.replace(/^+/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+        document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
     });
 };
 
