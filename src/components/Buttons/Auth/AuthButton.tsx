eb2e9e7f29fb390addafdc5b61ab6bc19c7be7c3
imtiazpy <ahmed.imti1994@gmail.com>
2022-10-24 15:33:33 +0600

    feat: added login functionality

--- src/components/Buttons/Auth/AuthButton.tsx
+++ src/components/Buttons/Auth/AuthButton.tsx
@@ -4,11 +4,10 @@ import AuthContext from '../../../context/Auth/AuthContext';
 export interface IAuthButton extends React.ComponentPropsWithoutRef<'button'> {}
 
 const AuthButton: React.FC<IAuthButton> = ({ className, ...buttonProps }) => {
-  const { authenticated, login, logout } = useContext(AuthContext);
+  const { authenticated, logout } = useContext(AuthContext);
 
   return (
     <button
-      onClick={authenticated ? logout : login}
       className={`${className} border-1 p-2 px-4 sm:px-6 bg-blue-500 rounded text-white w-28`}
       {...buttonProps}
     >
