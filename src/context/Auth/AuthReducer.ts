43588abb13b3c5dedea2ef558a4d2c0abd3be016
imtiazpy <ahmed.imti1994@gmail.com>
2022-12-18 19:57:56 +0600

    refactor: working on changing usestates to usereducer in authcontext

---
+++ src/context/Auth/AuthReducer.ts
@@ -0,0 +1,11 @@
+const reducer = (state: any, action: any) => {
+    switch (action.type) {
+        case 'LOGIN_SUCCESS':
+            return {
+                ...state,
+                
+            }
+    }
+}
+
+export default reducer;
